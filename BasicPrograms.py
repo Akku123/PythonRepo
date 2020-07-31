8278fc98c567f3a9d4c584a62503d22c6cd52a82
Akash Gupta <guptaak@INTEGRICHAIN.LOCAL>
2020-07-31 15:19:27 +0530

    Fifth Commit

--- BasicPrograms.py
+++ BasicPrograms.py
@@ -25,19 +25,19 @@ print("1.Add")
 print("2.Subtract")
 print("3.Divide")
 
-choice = input('Enter Choice 1/2/3:')
-if choice in ('1', '2', '3', '4'):
-    a = int(input("Enter value for first number:"))
-    b = int(input("Enter value for second number:"))
-    if choice == '1':
-        print(a, "+", b, "=", add(a, b))
-    elif choice == '2':
-        print(a, "-", b, "=", subtract(a, b))
-    elif choice == '3':
-        print(a, "/", b, "=", divide(a, b))
-
-else:
-    print('invalid input')
+# choice = input('Enter Choice 1/2/3:')
+# if choice in ('1', '2', '3', '4'):
+#     a = int(input("Enter value for first number:"))
+#     b = int(input("Enter value for second number:"))
+#     if choice == '1':
+#         print(a, "+", b, "=", add(a, b))
+#     elif choice == '2':
+#         print(a, "-", b, "=", subtract(a, b))
+#     elif choice == '3':
+#         print(a, "/", b, "=", divide(a, b))
+#
+# else:
+#     print('invalid input')
 
 
 
@@ -47,33 +47,35 @@ while(a<10):
     print(a)
     a = a+1
 
-correctNum = 13
-choice = int(input("Please enter a number"))
-if choice < correctNum:
-        print("number is too small")
-elif choice > correctNum:
-        print("number is too large")
-elif choice == correctNum:
-        print("congratulations")
+assert 5<4 , "5 is not less than 3"
 
-
-fruits = ['Mangoes','Bananan','Apple']
-
-for fruit in fruits:
-    print("fruit in list is",fruit)
-
-print("no fruits left in the list")
-
-num = int(input("Please enter a Number"))
-fac = 1
-if num<0:
-    print("Number should be positive")
-elif num==0:
-    print("Factorial is 1")
-else:
-    for i in range(1, num+1):
-     fac = fac*i
-    print(fac)
+# correctNum = 13
+# choice = int(input("Please enter a number"))
+# if choice < correctNum:
+#         print("number is too small")
+# elif choice > correctNum:
+#         print("number is too large")
+# elif choice == correctNum:
+#         print("congratulations")
+#
+#
+# fruits = ['Mangoes','Bananan','Apple']
+#
+# for fruit in fruits:
+#     print("fruit in list is",fruit)
+#
+# print("no fruits left in the list")
+#
+# num = int(input("Please enter a Number"))
+# fac = 1
+# if num<0:
+#     print("Number should be positive")
+# elif num==0:
+#     print("Factorial is 1")
+# else:
+#     for i in range(1, num+1):
+#      fac = fac*i
+#     print(fac)
 
 
 
