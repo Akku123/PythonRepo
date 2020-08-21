787ae374aeadb5d688addb7b1b9bed702d76b5c8
Akash Gupta <guptaak@INTEGRICHAIN.LOCAL>
2020-08-21 19:43:20 +0530

    Message

--- sqlconnection.py
+++ sqlconnection.py
@@ -1,17 +1,18 @@
 import pyodbc
 
-server = 'x.x.x.x'
-database = 'DB_Name'
-username = 'XXXX'
-password = 'XXXX'
+server = '20.20.1.20'
+database = 'X941Q1_DERIVE'
+username = 'agupta'
+password = 'Admin@12345678'
+print("X")
 
 
 conn = pyodbc.connect(
-    'DRIVER={DB DRIVER};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    'DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 def read(conn):
     print("Read")
     cursor = conn.cursor()
-    cursor.execute("SELECT * DATA TABLE")
+    cursor.execute("SELECT * FROM GTN_TYPE_LOOKUP")
     for row in cursor:
         print(row)
         print()
